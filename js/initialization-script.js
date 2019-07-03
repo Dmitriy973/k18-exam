79002acc57f6afb571a2a7499e8996f8dab19453
Dmitriy973 <cpmnach@gmail.com>
2019-07-03 21:43:23 +0300

    menu and slider

---
+++ js/initialization-script.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+customPaging : function(slider, i) {
+},
+dots:true,
+fade:true,
+slidesToShow:1,
+slidesToScrole:1,
+infinite: true,
+autoplay:true,
+autoplaySpeed:4500,
+arrows: true
+});
